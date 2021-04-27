477d76e183807936d437a63c87bf7cd053bb1238
ROUAHM <Roua.hammami@yahoo.com>
2021-04-28 01:10:44 +0200

    mise-a-jour

---
+++ src/main/java/com/star/gestionepave_back/GestionepaveBackApplication.java
@@ -0,0 +1,13 @@
+package com.star.gestionepave_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionepaveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionepaveBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/star/gestionepave_back/GestionepaveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.gestionepave_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionepaveBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
